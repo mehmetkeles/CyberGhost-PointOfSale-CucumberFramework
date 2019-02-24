2fd6ad2be01852b346eae82527e47ed94a767a2a
dserkan0@gmail.com <dserkan0@Gmail.com>
2019-02-23 17:47:26 -0700

    serkan order cucumber

--- src/test/java/com/briteerp/step_definitions/OrdersPageStepDefinitions.java
+++ src/test/java/com/briteerp/step_definitions/OrdersPageStepDefinitions.java
@@ -1,4 +1,35 @@
 package com.briteerp.step_definitions;
 
+import com.briteerp.utilities.ApplicationConstants;
+import com.briteerp.utilities.BrowserUtils;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+
 public class OrdersPageStepDefinitions extends UiCommon {
+
+    private String orderName = null;
+    @When("user clicks on orders link")
+    public void user_clicks_on_Products_link() {
+
+        WebElement element = pages.orders().ordersLink;
+        BrowserUtils.waitForClickablility(element, timeOutInSec);
+        element.click();
+        BrowserUtils.waitUntilTextToBePresentInElement(pages.products().tabTitle, ApplicationConstants.ORDERLISTS_PAGE_HEADER, timeOutInSec);
+        System.out.println("User clicks on the Orders link");
+    }
+    @Then("Orders page should be displayed")
+    public void orders_page_should_be_displayed() {
+        BrowserUtils.waitUntilTextToBePresentInElement(pages.products().tabTitle,
+                ApplicationConstants.ORDERLISTS_PAGE_HEADER,timeOutInSec);
+        Assert.assertEquals(pages.products().tabTitle.getText().trim(),
+                ApplicationConstants.ORDERLISTS_PAGE_HEADER);
+        System.out.println("Orders page is displayed");
+    }
+
+
+
+
 }
+
---
+++ src/test/resources/features/orderspage.feature
@@ -0,0 +1,16 @@
+@Orders
+Feature: Products functionality
+
+#  Background: getting to the Point of Sale Page as user
+#    Given user is already on the login page
+#    When user enters "in_pos_user4@info.com" and "KjKtfgrs40"
+#    Then homepage should be displayed
+#    When user clicks on PointofSale link
+#    Then PointOfSale page should be displayed
+
+
+  @wip
+  Scenario: valid OrdersPage Title
+    Given user is on the Point of Sale page
+    When user clicks on orders link
+    Then Orders page should be displayed
