d2ae5604fb23641f1b1c116233f6ef7fccf05eae
Mehmet <mhmtklsh@gmail.com>
2019-03-19 12:32:15 -0500

    database tests are added

--- src/test/java/com/briteerp/pages/PricelistPage.java
+++ src/test/java/com/briteerp/pages/PricelistPage.java
@@ -1,5 +1,7 @@
 package com.briteerp.pages;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -36,6 +38,25 @@ public class PricelistPage extends BasePage {
     @FindBy(xpath = "//div/p[2]")
     public WebElement text;
 
+    @FindBy(xpath = "//button[@class='btn btn-primary btn-sm o_list_button_add']")
+    public WebElement createButton;
+
+    @FindBy(xpath = "//input[@class='o_field_char o_field_widget o_input o_required_modifier']")
+    public WebElement pricelistNameInput;
+
+    @FindBy(xpath = "//button[@class='btn btn-primary btn-sm o_form_button_save']")
+    public WebElement saveButton;
+
+    @FindBy(xpath = "(//button[@class='o_dropdown_toggler_btn btn btn-sm dropdown-toggle'])[3]")
+    public WebElement managerActionButton;
+
+    @FindBy(linkText = "Delete")
+    public WebElement deleteButton;
+
+    @FindBy(xpath = "//button[@class='btn btn-sm btn-primary']")
+    public WebElement okButton;
+
+
     public PricelistPage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
@@ -50,6 +71,15 @@ public class PricelistPage extends BasePage {
         int rnd = new Random().nextInt(PriceListNameCheckBoxes.size());
         WebElement PricelistNameCheckBox = PriceListNameCheckBoxes.get(rnd);
         return PricelistNameCheckBox;
+    }
+
+    public boolean checkPricelistname(List<Object> pricelistNames, String pricelistName) {
+        for (Object name : pricelistNames) {
+            if (name.toString().equals(pricelistName)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
--- src/test/java/com/briteerp/step_definitions/DB_PricelistPageStepDefinitions.java
+++ src/test/java/com/briteerp/step_definitions/DB_PricelistPageStepDefinitions.java
@@ -1,4 +1,53 @@
 package com.briteerp.step_definitions;
 
-public class DB_PricelistPageStepDefinitions {
+import com.briteerp.utilities.BrowserUtils;
+import com.briteerp.utilities.DatabaseUtility;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
+
+import java.util.List;
+
+public class DB_PricelistPageStepDefinitions extends UiCommon {
+
+    @Then("manager clicks on the create button")
+    public void manager_clicks_on_the_create_button() {
+        pages.pricelist().createButton.click();
+        BrowserUtils.wait(5);
+    }
+
+    @When("manager enters a {string} and clicks on save button")
+    public void manager_enters_a_and_clicks_on_save_button(String pricelistName) {
+        pages.pricelist().pricelistNameInput.sendKeys(pricelistName);
+        pages.pricelist().saveButton.click();
+        BrowserUtils.wait(5);
+    }
+
+    @Then("manager verifies that new {string} is in database")
+    public void manager_verifies_that_new_is_in_database(String pricelistName) {
+        String query = "SELECT * FROM product_pricelist;";
+        List<Object> pricelistNames = DatabaseUtility.getColumnData(query, "name");
+        System.out.println(pricelistNames);
+
+        Assert.assertTrue(pages.pricelist().checkPricelistname(pricelistNames, pricelistName));
+
+    }
+
+    @Then("manager deletes that new {string} on pricelist page")
+    public void manager_deletes_that_new_on_pricelist_page(String pricelistName) {
+        pages.pricelist().managerActionButton.click();
+        pages.pricelist().deleteButton.click();
+        pages.pricelist().okButton.click();
+        BrowserUtils.wait(5);
+
+    }
+
+    @Then("manager verifies that new {string} is not in database")
+    public void manager_verifies_that_new_is_not_in_database(String pricelistName) {
+        String query = "SELECT * FROM product_pricelist;";
+        List<Object> pricelistNames = DatabaseUtility.getColumnData(query, "name");
+        System.out.println(pricelistNames);
+
+        Assert.assertFalse(pages.pricelist().checkPricelistname(pricelistNames, pricelistName));
+    }
 }
